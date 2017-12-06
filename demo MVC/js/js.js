a482ff7ca9f317de07772e812f502892eab84954
locnh0507 <33018902+locnh0507@users.noreply.github.com>
2017-12-06 21:47:36 +0700

    update

--- demo MVC/js/js.js
+++ demo MVC/js/js.js
@@ -1,80 +1,123 @@
 $(document).ready(function () {
     var model = {
-        dog: [{
-            name: 'Anh1',
+        coffee: [{
+            name: 'coffee 1',
             counter: 0,
             src: 'img/Anh1.png'
         },
         {
-            name: 'Anh2',
+            name: 'coffee 2',
             counter: 0,
             src: 'img/Anh2.png'
         },
         {
-            name: 'Anh3',
+            name: 'coffee 3',
             counter: 0,
             src: 'img/Anh3.png'
         }, {
-            name: 'Anh4',
+            name: 'coffee 4',
             counter: 0,
             src: 'img/Anh4.png'
         }, {
-            name: 'Anh5',
+            name: 'coffee 5',
             counter: 0,
             src: 'img/Anh5.png'
         },
         ],
 
-        currentDog:{
-            name: 'coffee',
-            src : 'img/anh1.png',
-            counter : 0
+        currentCoffee: {
+            name: 'coffee 1',
+            src: 'img/anh1.png',
+            counter: 0
         }
     }
 
-    var controller ={
-        getListName: function(){
-            var dogs= model.dog;
-            console.log(dogs);
+    var controller = {
+        getListName: function () {
+            var coffees = model.coffee;
+            // console.log(coffees);
             var names = [];
-            for(i=0; i<dogs.length;i++){
-                names.push(dogs[i].name);
+            for (i = 0; i < coffees.length; i++) {
+                names.push(coffees[i].name);
             }
-            
+
             return names;
         },
 
-        getCurrentDogs:function(){
-            return model.currentDog;
+        getcurrentCoffees: function () {
+            return model.currentCoffee;
         },
-        getImages:function(){
-            
+        getSrc: function () {
+            var src = model.coffee;
+            var srcs = [];
+            for (i = 0; i < src.length; i++) {
+                srcs.push(src[i].src);
+            }
+            // console.log(srcs)
+            return srcs;
         },
 
-        batdau:function(){
+        getCounter: function () {
+            var counter = model.coffee;
+            var x = $('img').attr('src')
+            console.log(x)        
+        },
+
+        batdau: function () {
             view.khoitao()
         },
     }
 
+    //var coffee = {};
+    //coffee.name = "abc";
     var view = {
-        khoitao:function(){
-            this.listDogNames = controller.getListName();
-            this.currentDog = controller.getCurrentDogs();
+
+        // view.thiscoffeeNames 
+        khoitao: function () {
+            
+            this.listcoffeeNames = controller.getListName();
+            this.currentCoffee = controller.getcurrentCoffees();
+            this.srcs = controller.getSrc();
             view.hienthi();
         },
 
-        hienthi:function(){
-            var listDogNames = this.listDogNames;
-            var html ='';
-            for(var i = 0; i < listDogNames.length; i++){
-                html += '<li data-name=' +i+ '>' + listDogNames[i]+ '</li>';
+        hienthi: function () {
+            var listcoffeeNames = this.listcoffeeNames;
+            var html = '';
+            for (var i = 0; i < listcoffeeNames.length; i++) {
+                html += '<li data-name=' + i + '>' + listcoffeeNames[i] + '</li>';
             }
             $('#list').html(html);
-            $('#name').text(this.currentDog.name);
-            $('#counter').text(this.currentDog.counter);
-            $('img').attr("src",this.currentDog.src);
-        } 
+            // $('#name').text(this.currentCoffee.name);
+            // $('#counter').text(this.currentCoffee.counter);
+            // $('img').attr("src", this.currentCoffee.src);
+            var srcs = this.srcs
+            // console.log(srcs);
+            $('li').click(function () {
+                var selected = $(this).attr('data-name')
+                $('img').attr('src', srcs[selected])
+                $('#name').text(listcoffeeNames[selected]);
+                $('#counter').text(model.coffee[selected].counter);
+            })
+
+            var arr = model.coffee;
+            $('#anh').click(function () {
+                var x = $('img').attr('src')
+                console.log(x)
+                for (i = 0; i < arr.length; i++) {
+                    if (x == arr[i].src) {
+                        arr[i].counter += 1;
+                        $('#counter').text(arr[i].counter);
+                    }
+                }
+            })
+            // $('img').click(function(){
+            //     like++;
+            //     $('#counter').text(like);
+            // })
+        }
 
     }
+
     controller.batdau();
 })
